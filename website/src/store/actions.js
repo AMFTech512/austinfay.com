6bd4eb6a66999ed684971ee9fae0c29a85eb3608
Austin <amftech512@gmail.com>
2019-11-26 14:24:01 -0700

    deployed first version to firebase

--- website/src/store/actions.js
+++ website/src/store/actions.js
@@ -3,12 +3,12 @@ export default {
         let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
         // Load the JSON from the API
         await context.app.$storyapi.get('cdn/stories', {
-            version: version
+            version: version,
+            cv: Math.floor(Date.now() / 60000)*60000
         }).then((res) => {
-            // console.log("Got the stories");
             context.store.commit('setStories', res.data.stories);
         }).catch((res) => {
-            context.error({ statusCode: res.response.status, message: res.response.data })
+            context.error({ statusCode: 500, message: res })
         })
     }
 };
