f8f400ae2ef3b9b021fd029790726b71a05320e2
Austin Fay <amftech512@gmail.com>
2019-11-28 19:48:19 -0700

    added header component

---
+++ website/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 :style="data.css">{{ data.text }}</h1>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
+
+<style>
+
+</style>
--- website/src/components/index.js
+++ website/src/components/index.js
@@ -1,9 +1,11 @@
 import NavBar from "@/components/NavBar";
 import Jumbotron from "@/components/Jumbotron";
-import DescCard from "@/components/DescCard.vue"
+import DescCard from "@/components/DescCard"
+import Header from "@/components/Header"
 
 export default {
     NavBar,
     Jumbotron,
-    DescCard
+    DescCard,
+    Header
 };
