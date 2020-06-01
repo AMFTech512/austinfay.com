7d39fee7535793426a6f03d38a0ed9d90746ffec
Austin <amftech512@gmail.com>
2020-05-31 19:50:06 -0600

    added identity widget to home

---
+++ website/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Welcome to Austinfay.com</h1>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            script: [
+                { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
